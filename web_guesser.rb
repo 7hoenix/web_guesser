cc82f7aebdef8da6a4e8a55b52bb76fe9e3d1381
philmphoenix <jpholzmann@gmail.com>
2015-08-16 20:46:07 -0600

    I4: Color extension implemented

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,16 +6,35 @@ SECRET_NUMBER = rand(101)
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  background = background_color(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message,
+    :background => background}
 end
 
-def check_guess(guess)
-  differential = (guess.to_i - SECRET_NUMBER).abs
+def background_color(guess)
+  if differential(guess) >= 5
+    "red"
+elsif differential(guess) == 0
+  "green"
+else
+  "yellow"
+  end
+end
+
+def differential(guess)
+  (guess.to_i - SECRET_NUMBER).abs
+end
+
+def pre_message(differential)
   if differential >= 5
     pre_message = "Way too "
   else
     pre_message = "Too "
   end
+end
+
+def check_guess(guess)
+  pre_message = pre_message(differential(guess))
   if guess.nil?
     message = "Take a guess"
   elsif SECRET_NUMBER < guess.to_i
